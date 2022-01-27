27aa9d812624b7b8c850dbe23ad1122756859fe6
AHMOUZOUNE <ahmed_mouzoune@carrefour.com>
2022-01-27 12:58:33 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-intro';
+  title = 'counter';
+  public count = 5;
+  product: any;
+  products: any;
+
+  constructor(private httpClient: HttpClient) {}
+
+  /**
+   * fetchProducts
+   */
+  public fetchProducts() {
+    this.httpClient.get('https://fakestoreapi.com/products')
+      .subscribe((res: any) => {
+        this.product = res[0];
+        this.products = res.slice(0,6);
+      });
+  }
+  /**
+   * add
+   */
+  public add() {
+    this.count++;
+    console.log('this.count', this.count);
+  }
+  /**
+   * minus
+   */
+  public minus() {
+    this.count--;
+    console.log('this.count', this.count);
+  }
 }
