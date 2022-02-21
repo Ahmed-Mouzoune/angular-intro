2393ba5ecd8b0a63dba9595840e89bf2242ceece
Ahmed Mouzoune <ahmedmouzoune@MacBook-Pro-de-Ahmed.local>
2022-02-21 12:49:41 +0100

    create product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() title: string | undefined;
+  @Input() description: string | undefined;
+  @Input() price: string | undefined;
+  @Input() image: string | undefined;
+  @Input() category: string | undefined;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
